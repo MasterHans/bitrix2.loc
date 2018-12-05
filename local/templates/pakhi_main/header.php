cec1dfc07b94c7fcf79d7de66dda7b63e90a4097
Master_Hans <suvorov.ag.75@gmail.com>
2018-12-05 10:15:15 +0300

    Меню 1

--- local/templates/pakhi_main/header.php
+++ local/templates/pakhi_main/header.php
@@ -53,42 +53,61 @@
 
                 <a href="cart.html"><i class="cart"></i></a>
             </div>
-            <div class="navigation">
-                <div>
-                    <label class="mobile_menu" for="mobile_menu">
-                        <span>Menu</span>
-                    </label>
-                    <input id="mobile_menu" type="checkbox">
-                    <ul class="nav">
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li class="dropdown1"><a href="#">Dresses</a>
-                            <ul class="dropdown2">
-                                <li><a href="products.html">Dress Materials</a></li>
-                                <li><a href="products.html">Kurta & Kurti</a></li>
-                                <li><a href="products.html">Sarees</a></li>
-                                <li><a href="products.html">Chudidars</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown1"><a href="#">Bags</a>
-                            <ul class="dropdown2">
-                                <li><a href="products.html">Latest</a></li>
-                                <li><a href="products.html">Leather Bags</a></li>
-                                <li><a href="products.html">Hand Bags</a></li>
-                            </ul>
-                        </li>
-                        </li>
-                        <li class="dropdown1"><a href="#">Shoes</a>
-                            <ul class="dropdown2">
-                                <li><a href="products.html">Sports Shoes</a></li>
-                                <li><a href="products.html">Casual Shoes</a></li>
-                                <li><a href="products.html">Formal Shoes</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="contact.html">Contact US</a></li>
-                        <div class="clearfix"></div>
-                    </ul>
-                </div>
-            </div>
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:menu",
+                "",
+                Array(
+                    "ALLOW_MULTI_SELECT" => "N",
+                    "CHILD_MENU_TYPE" => "left",
+                    "DELAY" => "N",
+                    "MAX_LEVEL" => "2",
+                    "MENU_CACHE_GET_VARS" => array(""),
+                    "MENU_CACHE_TIME" => "3600",
+                    "MENU_CACHE_TYPE" => "N",
+                    "MENU_CACHE_USE_GROUPS" => "Y",
+                    "ROOT_MENU_TYPE" => "main",
+                    "USE_EXT" => "Y"
+                )
+            );?>
+<!--            <div class="navigation">-->
+<!--                <div>-->
+<!--                    <label class="mobile_menu" for="mobile_menu">-->
+<!--                        <span>Menu</span>-->
+<!--                    </label>-->
+<!--                    <input id="mobile_menu" type="checkbox">-->
+<!--                    <ul class="nav">-->
+<!--                        <li class="active"><a href="index.html">Home</a></li>-->
+<!--                        <li class="dropdown1"><a href="#">Dresses</a>-->
+<!--                            <ul class="dropdown2">-->
+<!--                                <li><a href="products.html">Dress Materials</a></li>-->
+<!--                                <li><a href="products.html">Kurta & Kurti</a></li>-->
+<!--                                <li><a href="products.html">Sarees</a></li>-->
+<!--                                <li><a href="products.html">Chudidars</a></li>-->
+<!--                            </ul>-->
+<!--                        </li>-->
+<!--                        <li class="dropdown1"><a href="#">Bags</a>-->
+<!--                            <ul class="dropdown2">-->
+<!--                                <li><a href="products.html">Latest</a></li>-->
+<!--                                <li><a href="products.html">Leather Bags</a></li>-->
+<!--                                <li><a href="products.html">Hand Bags</a></li>-->
+<!--                            </ul>-->
+<!--                        </li>-->
+<!--                        </li>-->
+<!--                        <li class="dropdown1"><a href="#">Shoes</a>-->
+<!--                            <ul class="dropdown2">-->
+<!--                                <li><a href="products.html">Sports Shoes</a></li>-->
+<!--                                <li><a href="products.html">Casual Shoes</a></li>-->
+<!--                                <li><a href="products.html">Formal Shoes</a></li>-->
+<!--                            </ul>-->
+<!--                        </li>-->
+<!--                        <li><a href="contact.html">Contact US</a></li>-->
+<!--                        <div class="clearfix"></div>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--            </div>-->
+
+
+
         </div>
     </div>
     <div class="banner">
